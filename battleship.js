c23f5a77962bff7a98a95824b259712eb39353ce
HuntDubs <Hunterwilson4092@gmail.com>
2021-09-09 12:24:41 -0500

    Drawing the boxes and Indexes

--- battleship.js
+++ battleship.js
@@ -3,12 +3,36 @@ let context;
 
 function draw(){
     context.clearRect(0,0,canvas.width,canvas.length);
+    
+    context.beginPath();
+    context.lineWidth = 2;
+    context.strokeStyle = "green";
+    context.rect(75,75,550,550);
+    context.stroke();
+    context.beginPath();
+    context.lineWidth = 2;
+    context.strokeStyle = "red";
+    context.rect(875,75,550,550);
+    context.stroke();
 
-    console.log("Test");
+    let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+    for(let x=0; x<10;x++){
+        context.font = "30px Verdana";
+        context.fillText(letters[x], 102.5 + 55*x, 70);
+        context.fillText(letters[x], 902.5 + 55*x, 70);
+    }
+    for(let x=1; x<=9; x++){
+        context.font = "30px Verdana";
+        context.fillText(x, 55, 105.5 + (x-1)*61.1);
+        context.fillText(x, 1425, 105.5 + (x-1)*61.1);
+    }
+    
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     canvas = document.querySelector("#myCanvas");
-    context = canvas.getContext("2d");
+    if (canvas != null){
+        context = canvas.getContext("2d");
+    } 
     draw();
   })
