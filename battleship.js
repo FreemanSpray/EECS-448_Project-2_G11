f1cc32c07eef8110fad2da2444cea7d4715d6241
mohit712319 <83716213+mohit712319@users.noreply.github.com>
2021-09-11 12:09:55 -0500

    Added click event

--- battleship.js
+++ battleship.js
@@ -85,3 +85,24 @@ document.addEventListener("DOMContentLoaded", () => {
     drawTemplate();
     drawStartUI();
   })
+
+function roundMegreenhorz(x){ return Math.floor(((x-85)/55))}
+function roundMegreenvert(y){return Math.floor(((y-85)/61.1))}
+function roundMeredhorz(x){ return Math.floor(((x-885)/55))}
+function roundMeredvert(y){return Math.floor(((y-85)/61.1))}
+document.addEventListener("click", e => {
+  const [i] = [e.x].map(roundMegreenhorz);
+  const [j] = [e.y].map(roundMegreenvert);
+  const[a]=[e.x].map(roundMeredhorz);
+  const[b]=[e.y].map(roundMeredvert);
+  if(i>=0 && i<10 && j>=0 && j<9)
+  {
+        alert("board 1");
+  }
+  
+  if(a>=0 && a<10 && b>=0 && b<9)
+  {
+        alert("board 2");
+  }
+        
+})
