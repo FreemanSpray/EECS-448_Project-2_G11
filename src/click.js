8f24668815954012d90f8753159e8fa43f082de6
mayank garg <mohitgar2001@gmail.com>
2021-09-16 16:10:37 -0500

    Added logic to store how many ships user wants based on button click.

--- src/click.js
+++ src/click.js
@@ -9,40 +9,39 @@ document.addEventListener("click", e => {
       const[b]=[e.y].map(roundMeredvert); 
 
       if (gameLogic.pickNumShips == true){
-            if(e.x>=660 && e.x<=860)
+            if(e.x>=660 && e.x<=860)//checks to see what button is clicked for number of Ships and set numShips to appropiate value
             {
                   if(e.y>=210 && e.y<=250)
                   {
-                        console.log("button 1")
+                        gameLogic.numShips=1;
                   }
                   if(e.y>=260 && e.y<=300)
                   {
-                        console.log("button 2")
+                        gameLogic.numShips=2;
+
                   }
                   if(e.y>=310 && e.y<=350)
                   {
-                        console.log("button 3")
+                        gameLogic.numShips=3;
                   }
                   if(e.y>=360 && e.y<=400)
                   {
-                        console.log("button 4")
+                        gameLogic.numShips=4;
                   }
                   if(e.y>=410 && e.y<=450)
                   {
-                        console.log("button 5")
+                        gameLogic.numShips=5;
                   }
                   if(e.y>=460 && e.y<=500)
                   {
-                        console.log("button 6")
+                        gameLogic.numShips=6;
+
                   }
             }
+            gameLogic.pickNumShips = false;
+            gameLogic.placing == true;
       }
-      //       // Check which button was clicked
 
-      //       gameLogic.numShips = // whichever button is clicked
-      //       gameLogic.pickNumShips = false;
-      //       gameLogic.placing == true;
-      // }
       if (GameLogic.placing == true){
             if(i>=0 && i<10 && j>=0 && j<9){
                   // for each ship needed to place, allow player to place, and then change to player 2 to allow placement
