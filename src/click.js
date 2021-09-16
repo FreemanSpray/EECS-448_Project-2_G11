52c47ff06ad624fba78021554f0753e4e179748a
Christian <elcman965@gmail.com>
2021-09-16 12:08:13 -0500

    added support for flags and different points of the game

--- src/click.js
+++ src/click.js
@@ -3,26 +3,34 @@ function roundMegreenvert(y){return Math.floor(((y-85)/61.1))}
 function roundMeredhorz(x){ return Math.floor(((x-885)/55))}
 function roundMeredvert(y){return Math.floor(((y-85)/61.1))}
 document.addEventListener("click", e => {
-  const [i] = [e.x].map(roundMegreenhorz);
-  const [j] = [e.y].map(roundMegreenvert);
-  const[a]=[e.x].map(roundMeredhorz);
-  const[b]=[e.y].map(roundMeredvert);
-  if(i>=0 && i<10 && j>=0 && j<9)
-  {
-        console.log(e.x,e.y)
-        //drawHMResult(j,i)
-        console.log(j,i)
-        
-        ship_front_tail.push([j,i])//push coordinates of click to an array that will hold the front and tail of a ship? then one array size is 2, place the ship?
-        //fire_missile([j,i],player1)
-  }
-  
-  if(a>=0 && a<10 && b>=0 && b<9)
-  {
-        console.log(b,a)
-        //drawHMResult(b,a)
-        fire_missile([b,a], player1)
+      const [i] = [e.x].map(roundMegreenhorz);
+      const [j] = [e.y].map(roundMegreenvert);
+      const[a]=[e.x].map(roundMeredhorz);
+      const[b]=[e.y].map(roundMeredvert);
+      // if (gameLogic.pickNumShips == true){
+      //       // Check which button was clicked
 
-  }
-        
+      //       gameLogic.numShips = // whichever button is clicked
+      //       gameLogic.pickNumShips = false;
+      //       gameLogic.placing == true;
+      // }
+      if (gameLogic.placing == true){
+            if(i>=0 && i<10 && j>=0 && j<9){
+                  // for each ship needed to place, allow player to place, and then change to player 2 to allow placement
+                  console.log(e.x,e.y)
+                  //drawHMResult(j,i)
+                  console.log(j,i)
+                  ship_front_tail.push([j,i])//push coordinates of click to an array that will hold the front and tail of a ship? then one array size is 2, place the ship?
+                  //fire_missile([j,i],player1)
+            }
+      }
+      else if (gameLogic.firing == true){
+            if(a>=0 && a<10 && b>=0 && b<9)
+            {
+                  console.log(b,a)
+                  //drawHMResult(b,a)
+                  fire_missile([b,a], player1)
+
+            }
+      }         
 })
