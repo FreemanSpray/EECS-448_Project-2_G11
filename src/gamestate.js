b78a3e6974d1d2aeb818b1d3641346b40e1182d7
Christian <elcman965@gmail.com>
2021-09-16 17:29:22 -0500

    added shipPlaced counter

--- src/gamestate.js
+++ src/gamestate.js
@@ -9,12 +9,12 @@ class Cell{
 class GameState{
     constructor(){
         this.grid = new Array(9);
+        this.shipsPlaced = 0;
         for(var i = 0; i < this.grid.length; i++){
             this.grid[i] = new Array(10);
             for(var j = 0; j < this.grid[i].length; j++){
                 this.grid[i][j] = new Cell();
             }
         }
-        console.log(this.grid);
     }
 }
