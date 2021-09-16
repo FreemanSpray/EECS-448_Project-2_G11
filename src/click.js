b323fa795b2a1ac780555dc7c1af20b87f844668
Christian <elcman965@gmail.com>
2021-09-16 17:28:58 -0500

    updated ship placing

--- src/click.js
+++ src/click.js
@@ -7,7 +7,6 @@ document.addEventListener("click", e => {
       const [j] = [e.y].map(roundMegreenvert);
       const[a]=[e.x].map(roundMeredhorz);
       const[b]=[e.y].map(roundMeredvert); 
-
       if (gameLogic.pickNumShips == true){
             if(e.x>=660 && e.x<=860)//checks to see what button is clicked for number of Ships and set numShips to appropiate value
             {
@@ -37,19 +36,59 @@ document.addEventListener("click", e => {
                         gameLogic.numShips=6;
 
                   }
+                  gameLogic.pickNumShips = false;
+                  gameLogic.placing = true;
+                  alert('Place your 1 length ship.')
             }
-            gameLogic.pickNumShips = false;
-            gameLogic.placing == true;
+            
       }
-
       else if (gameLogic.placing == true){
             if(i>=0 && i<10 && j>=0 && j<9){
                   if (gameLogic.player1Turn == true){
-                        // peter place a ship on the specific players board after validating that the ship placement is valid
+                        if(player1.board.shipsPlaced < gameLogic.numShips){
+                              if(player1.board.shipsPlaced == 0){
+                                    place_ship([j,i],[j,i],player1);
+                                    player1.board.shipsPlaced++;
+                              }
+                              else if(ship_front_tail.length == 2){
+                                    place_ship(ship_front_tail[0],ship_front_tail[1],player1);
+                                    ship_front_tail = [];
+                                    player1.board.shipsPlaced++;
+                              }
+                              else{
+                                    ship_front_tail.push([j,i]);  
+                              }
+                        }
+                        else{
+                              gameLogic.player1Turn = false;
+                        }
+                  }
+                  else if (gameLogic.player1Turn == false){
+                        if(player2.board.shipsPlaced < gameLogic.numShips){
+                              if(player2.board.shipsPlaced == 0){
+                                    place_ship([j,i],[j,i],player2);
+                                    player2.board.shipsPlaced++;
+                              }
+                              else if(ship_front_tail.length == 2){
+                                    place_ship(ship_front_tail[0],ship_front_tail[1],player2);
+                                    ship_front_tail = [];
+                                    player2.board.shipsPlaced++;
+                              }
+                              else{
+                                    ship_front_tail.push([j,i]);  
+                              }
+                        }
+                        else{
+                              gameLogic.placing = false;
+                              gameLogic.firing = true;
+                        }
+                        
                   }
             }
+            console.log(player1.board)
       }
       else if (gameLogic.firing == true){
+            console.log("fire!")
             if(a>=0 && a<10 && b>=0 && b<9)
             {
                   if (gameLogic.player1Turn == true){
