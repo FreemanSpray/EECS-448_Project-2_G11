0c3cf2971e028a9a2da23eba12cfadc99433be86
mayank garg <mohitgar2001@gmail.com>
2021-09-19 20:20:12 -0500

    close to done with functions for playerswitching

--- src/game.js
+++ src/game.js
@@ -6,13 +6,14 @@ class GameLogic{
         this.player2Turn = true;
         this.firing = false;
         this.numShips = 1;
+        this.transitionturn=0;//variable that equals 1 when transitioning between turns during game
     }
 }
 let gameLogic = new GameLogic();
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {  alert("Pick how many ships you'd like to play with!");   }, 300);
 })
-let endshipplacing_helper=0;
+
  
 function endTurnshipplacing(x,y){
     if (x>=650 && x<=850 && y >=300 && y <=340 ){
@@ -43,6 +44,7 @@ function endTurnshipplacing(x,y){
     }
     
 }
+let endshipplacing_helper=0;//variable that equals 1 to indicate shipplacing has ended
 function transition(x,y){
     if (x>=650 && x<=850 && y >=300 && y <=340 && gameLogic.placing==false && gameLogic.firing==false)
     {
@@ -63,23 +65,34 @@ function transition(x,y){
 }
 
 
+
+
 function endTurngame(x,y)
 {
     if (x>=650 && x<=850 && y >=300 && y <=340 && gameLogic.placing==false && gameLogic.firing==true)
     {
-        if(gameLogic.player1Turn == true)
+        if(gameLogic.player1Turn == true && gameLogic.player2Turn==true)
         {
-            console.log("1turn")
+            //console.log("1turn")
             drawTemplate();
             gameLogic.player1Turn=false;
+            gameLogic.transitionturn=1;
             //drawStartTurnButton();
         }
          //console.log(gameLogic.player1Turn,gameLogic.player2Turn)
-        else if(gameLogic.player1Turn==false)
+        else if(gameLogic.player1Turn==false && gameLogic.player2Turn==true && gameLogic.transitionturn==1)
         {
-            console.log("2turn");
+            //console.log("2turn");
             drawStartTurnButton();
+            gameLogic.transitionturn=0;
+
         }
+        else if(gameLogic.player1Turn==false && gameLogic.player2Turn==true && gameLogic.transitionturn==0)
+        {
+            drawTemplate();
+            boardfreezestate=0;
+        }
+        //if(gameLogic.)
 
 
     }
